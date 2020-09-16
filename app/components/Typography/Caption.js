67798e9f481562b369877e0368d140506302e4e9
Divy Jain <dkj@somaiya.edu>
2020-09-16 16:10:21 +0530

    💄 update typography

--- app/components/Typography/Caption.js
+++ app/components/Typography/Caption.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import { Caption, useTheme } from 'react-native-paper';
 
-export default function ({ children, style }) {
+export default function ({ children, style, small = false }) {
   const theme = useTheme();
 
   return (
     <Caption
       style={{
-        fontSize: 16,
+        fontSize: small ? 12 : 16,
         lineHeight: 24,
         letterSpacing: 0,
         color: theme.colors.grey,
