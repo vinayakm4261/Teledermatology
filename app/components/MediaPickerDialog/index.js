5aa0ab81989800e45ace42bf82370a80dd79cf36
Divy Jain <dkj@somaiya.edu>
2020-09-16 15:57:30 +0530

    🎨 restructure media picker dialog

--- app/components/MediaPickerDialog/index.js
+++ app/components/MediaPickerDialog/index.js
@@ -1,139 +1,147 @@
-import React from 'react';
+import React, { useMemo, useCallback } from 'react';
 import { View } from 'react-native';
-import { useTheme, Portal } from 'react-native-paper';
-import Modal from 'react-native-modal';
+import { FAB } from 'react-native-paper';
 import ImagePicker from 'react-native-image-crop-picker';
 
-import formStyles from '../../forms/styles';
+import Label from '../Typography/Label';
 import Subheading from '../Typography/Subheading';
-import Button from '../Button';
+import BottomModal from '../BottomModal';
 import useAudioRecorderDialog from '../../hooks/useAudioRecorderDialog';
 
-export default function ({ visible, onDismiss, onPicked, onError }) {
-  const theme = useTheme();
-  const {
-    showDialog: showAudioRecorderDialog,
-    dialog: audioRecorderDialog,
-  } = useAudioRecorderDialog({
-    onError,
-    onRecorded: (file) => {
-      onPicked([{ uri: `file://${file}`, type: 'audio' }]);
+const noOp = () => {};
+
+export default ({ visible, onDismiss, onPicked, onError }) => {
+  const handlePicked = useCallback(
+    (media) => {
+      onDismiss();
+      onPicked(media);
     },
+    [onDismiss, onPicked],
+  );
+
+  const handleAudioRecorded = useCallback(
+    (file) => {
+      handlePicked([{ uri: `file://${file}`, type: 'audio' }]);
+    },
+    [handlePicked],
+  );
+
+  const audioRecorder = useAudioRecorderDialog({
+    onError,
+    onRecorded: handleAudioRecorded,
   });
+
+  const captureImage = useCallback(() => {
+    ImagePicker.openCamera({
+      multiple: true,
+      compressImageQuality: 0.75,
+      mediaType: 'photo',
+      cropping: true,
+    })
+      .then((images) => {
+        handlePicked(
+          images.map((image) => ({
+            uri: image.path,
+            type: 'image',
+          })),
+        );
+      })
+      .catch(noOp);
+  }, [handlePicked]);
+
+  const captureVideo = useCallback(() => {
+    ImagePicker.openCamera({
+      multiple: true,
+      mediaType: 'video',
+    })
+      .then((videos) => {
+        handlePicked(
+          videos.map((video) => ({
+            uri: video.path,
+            type: 'video',
+          })),
+        );
+      })
+      .catch(noOp);
+  }, [handlePicked]);
+
+  const selectFromGallery = useCallback(() => {
+    ImagePicker.openPicker({
+      multiple: true,
+      compressImageQuality: 0.75,
+      mediaType: 'any',
+    })
+      .then((files) => {
+        handlePicked(
+          files.map((file) => ({
+            uri: file.path,
+            type: file.mime.split('/')[0],
+          })),
+        );
+      })
+      .catch(noOp);
+  }, [handlePicked]);
+
+  const buttons = useMemo(
+    () => [
+      {
+        icon: 'camera',
+        label: 'PHOTO',
+        onPress: captureImage,
+      },
+      {
+        icon: 'video',
+        label: 'VIDEO',
+        onPress: captureVideo,
+      },
+      {
+        icon: 'image',
+        label: 'GALLERY',
+        onPress: selectFromGallery,
+      },
+      {
+        icon: 'microphone',
+        label: 'VOICE',
+        onPress: audioRecorder.showDialog,
+      },
+    ],
+    [captureImage, captureVideo, selectFromGallery, audioRecorder],
+  );
 
   return (
     <>
-      <Portal>
-        <Modal
-          useNativeDriver
-          hideModalContentWhileAnimating
-          backdropOpacity={0.25}
-          style={{
-            justifyContent: 'flex-end',
-            padding: 0,
-            margin: 0,
-          }}
-          isVisible={visible}
-          swipeDirectvision={['down']}
-          onBackdropPress={onDismiss}
-          onBackButtonPress={onDismiss}
-          onModalHide={onDismiss}
-          onSwipeComplete={onDismiss}>
-          <View
-            style={{
-              margin: 8,
-              padding: 12,
-              backgroundColor: 'white',
-              zIndex: 100,
-              borderRadius: theme.roundness * 2,
-            }}>
-            <Subheading>Select Media Type</Subheading>
-            <View style={[formStyles.inputRow, { marginBottom: 8 }]}>
-              <Button
-                compact
-                style={formStyles.inputRowLeft}
-                icon="camera"
-                onPress={() => {
-                  ImagePicker.openCamera({
-                    multiple: true,
-                    compressImageQuality: 0.75,
-                    mediaType: 'photo',
-                    cropping: true,
-                  }).then((images) => {
-                    onDismiss();
-                    console.log(images);
-                    onPicked(
-                      images.map((image) => ({
-                        uri: image.path,
-                        type: 'image',
-                      })),
-                    );
-                  });
-                }}
-                mode="contained">
-                Photo
-              </Button>
-              <Button
-                compact
-                style={formStyles.inputRowRight}
-                icon="video"
-                onPress={() => {
-                  ImagePicker.openCamera({
-                    multiple: true,
-                    mediaType: 'video',
-                  }).then((videos) => {
-                    onDismiss();
-                    onPicked(
-                      videos.map((video) => ({
-                        uri: video.path,
-                        type: 'video',
-                      })),
-                    );
-                  });
-                }}
-                mode="contained">
-                Video
-              </Button>
+      <BottomModal {...{ visible, onDismiss }}>
+        <Subheading>Select Media Type</Subheading>
+        <View style={styles.buttonsGroup}>
+          {buttons.map(({ icon, label, onPress }) => (
+            <View key={icon} style={styles.buttonContainer}>
+              <FAB icon={icon} style={styles.button} onPress={onPress} />
+              <Label style={styles.bottonLabel}>{label}</Label>
             </View>
-            <View style={formStyles.inputRow}>
-              <Button
-                compact
-                style={formStyles.inputRowLeft}
-                icon="image"
-                onPress={() => {
-                  ImagePicker.openPicker({
-                    multiple: true,
-                    compressImageQuality: 0.75,
-                    mediaType: 'any',
-                  }).then((files) => {
-                    onDismiss();
-                    onPicked(
-                      files.map((file) => ({
-                        uri: file.path,
-                        type: file.mime.split('/')[0],
-                      })),
-                    );
-                  });
-                }}
-                mode="contained">
-                Gallery
-              </Button>
-              <Button
-                compact
-                style={formStyles.inputRowRight}
-                icon="microphone"
-                onPress={() => {
-                  onDismiss();
-                  showAudioRecorderDialog();
-                }}
-                mode="contained">
-                Voice
-              </Button>
-            </View>
-          </View>
-        </Modal>
-      </Portal>
-      {audioRecorderDialog}
+          ))}
+        </View>
+      </BottomModal>
+      {audioRecorder.dialog}
     </>
   );
-}
+};
+
+const styles = {
+  buttonsGroup: {
+    marginBottom: 8,
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  buttonContainer: {
+    margin: 8,
+    alignItems: 'center',
+    flexGrow: 1,
+  },
+  button: {
+    elevation: 1,
+  },
+  bottonLabel: {
+    margin: 0,
+  },
+};
