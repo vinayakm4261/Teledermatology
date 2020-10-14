6b09a1c127375db8d740587889b88ab033894386
Divy Jain <dkj@somaiya.edu>
2020-10-14 15:51:38 +0530

    💄 update recorder dialog ui

--- app/components/AudioRecorderDialog/index.js
+++ app/components/AudioRecorderDialog/index.js
@@ -1,10 +1,11 @@
 import React, { useCallback, useState } from 'react';
 import { View } from 'react-native';
-import { Text, ProgressBar, IconButton } from 'react-native-paper';
+import { FAB, useTheme } from 'react-native-paper';
 
 import BottomModal from '../BottomModal';
 import Button from '../Button';
 import Subheading from '../Typography/Subheading';
+import Caption from '../Typography/Caption';
 import formStyles from '../../forms/styles';
 import useAudioRecorder from '../../hooks/useAudioRecorder';
 import useAudioPlayer from '../../hooks/useAudioPlayer';
@@ -21,8 +22,9 @@ const AudioRecorderDialog = ({
   onRecorded,
   onError,
 }) => {
+  const theme = useTheme();
   const [filepath, setFilepath] = useState(null);
-  const [stage, setStage] = useState('READY');
+  const [stage, setStage] = useState(stages.READY);
 
   const recorder = useAudioRecorder({
     onError,
@@ -44,40 +46,53 @@ const AudioRecorderDialog = ({
 
   return (
     <BottomModal {...{ visible, onDismiss: handleDismiss }}>
+      <Subheading style={{ alignSelf: 'center' }}>Record Audio</Subheading>
       {stage === stages.RECORDING ? (
         <>
-          <Subheading style={{ marginBottom: 12 }}>Recording</Subheading>
-          <View style={formStyles.inputRow}>
-            <Text style={formStyles.inputRowLeft}>Recording</Text>
-            <Button
-              compact
-              style={formStyles.inputRowRight}
-              theme={{ colors: { primary: '#c4001d' } }}
-              icon="stop"
-              onPress={recorder.endRecording}
-              mode="contained">
-              Stop
-            </Button>
+          <Caption small style={{ alignSelf: 'center', marginBottom: 12 }}>
+            Recording... {recorder.currentTime}
+          </Caption>
+          <View style={styles.buttonsGroup}>
+            <View style={styles.buttonContainer}>
+              <FAB
+                icon="stop"
+                style={styles.button}
+                onPress={recorder.endRecording}
+                theme={{ colors: { accent: 'red' } }}
+                color="white"
+              />
+              <Caption small style={{ ...styles.bottonLabel, color: 'red' }}>
+                STOP RECORDING
+              </Caption>
+            </View>
           </View>
         </>
       ) : stage === stages.PREVIEW ? (
         <>
-          <Subheading style={{ marginBottom: 12 }}>Preview</Subheading>
-          <View style={formStyles.inputGroup}>
-            <ProgressBar progress={player.currentTime / player.duration} />
-            <IconButton
-              icon={player?.isPlaying ? 'stop' : 'play'}
-              onPress={() => {
-                if (player?.isPlaying) player.stop();
-                else player.play();
-              }}
-            />
+          <Caption small style={{ alignSelf: 'center', marginBottom: 12 }}>
+            {player?.isPlaying
+              ? `Playing... ${player?.currentTime ? player.currentTime : ''}`
+              : 'Press play button to preview recording.'}
+          </Caption>
+          <View style={styles.buttonsGroup}>
+            <View style={styles.buttonContainer}>
+              <FAB
+                icon={player?.isPlaying ? 'stop' : 'play'}
+                style={styles.button}
+                onPress={recorder.endRecording}
+                color="white"
+              />
+              <Caption
+                small
+                style={{ ...styles.bottonLabel, color: theme.colors.accent }}>
+                {player?.isPlaying ? 'STOP PLAYING' : 'PLAY RECORDING'}
+              </Caption>
+            </View>
           </View>
           <View style={formStyles.inputRow}>
             <Button
               compact
               style={formStyles.inputRowLeft}
-              icon="restart"
               onPress={() => {
                 player.stop();
                 setStage(stages.READY);
@@ -88,7 +103,6 @@ const AudioRecorderDialog = ({
             <Button
               compact
               style={formStyles.inputRowRight}
-              icon="check"
               onPress={() => {
                 onRecorded(filepath);
                 handleDismiss();
@@ -100,29 +114,49 @@ const AudioRecorderDialog = ({
         </>
       ) : (
         <>
-          <Subheading style={{ marginBottom: 12 }}>Audio</Subheading>
-          <View style={formStyles.inputRow}>
-            <Button
-              compact
-              style={formStyles.inputRowLeft}
-              icon="close"
-              onPress={handleDismiss}
-              mode="outlined">
-              Cancel
-            </Button>
-            <Button
-              compact
-              style={formStyles.inputRowRight}
-              icon="microphone"
-              onPress={recorder.startRecording}
-              mode="contained">
-              Record
-            </Button>
+          <Caption style={{ alignSelf: 'center', marginBottom: 12 }} small>
+            Press record button to start recording.
+          </Caption>
+          <View style={styles.buttonsGroup}>
+            <View style={styles.buttonContainer}>
+              <FAB
+                icon="microphone"
+                style={styles.button}
+                onPress={recorder.startRecording}
+                theme={{ colors: { accent: theme.colors.primary } }}
+                color="white"
+              />
+              <Caption
+                small
+                style={{ ...styles.bottonLabel, color: theme.colors.primary }}>
+                START RECORDING
+              </Caption>
+            </View>
           </View>
         </>
       )}
     </BottomModal>
   );
+};
+
+const styles = {
+  buttonsGroup: {
+    margin: 8,
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  buttonContainer: {
+    margin: 8,
+    alignItems: 'center',
+    flexGrow: 1,
+  },
+  button: {
+    elevation: 0,
+  },
+  bottonLabel: {
+    margin: 0,
+  },
 };
 
 export default AudioRecorderDialog;
